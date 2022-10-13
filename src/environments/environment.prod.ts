364e197160db28659148ef90de726a8d028065b4
qbhasha <115475262+qbhasha@users.noreply.github.com>
2022-10-13 12:31:30 +0100

    Update environment.prod.ts
    Updated the backend access url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "https://lol-champions-ose9.apps.ose.openshift.conygre.com/api/v1/lolchampion/"
+  apiUrl: "https://lol-champions-ose9-champs.apps.ose.openshift.conygre.com/api/v1/lolchampion/"
 };
